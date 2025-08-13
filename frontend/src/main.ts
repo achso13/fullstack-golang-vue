27ca7247ee93e0b759180e0a7f6583be4710354f
achmadsolehuddin <139105322+achmadsolehuddin@users.noreply.github.com>
2025-08-13 15:41:37 +0700

    feat: (frontend) login, register

---
+++ frontend/src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { VueQueryPlugin } from '@tanstack/vue-query'
+import routes from './routes'
+
+const app = createApp(App)
+
+app.use(VueQueryPlugin)
+
+app.use(routes)
+
+app.mount('#app')
